1327df7d7d934bcc79d381ed8fcd4d7b59f29ea4
Daniel Hipolito Tolentino <hipolito_bougs@hotmail.com>
2023-10-08 16:35:38 -0600

    Added Simple form

---
+++ pages/form.js
@@ -0,0 +1,19 @@
+const Form = () =>  {
+    console.error(`dd`);
+    return <div>
+        <form action="mailto:recipient@example.com" method="get" enctype="text/plain">
+        Subject:<br/>
+    <input type="text" name="subject" placeholder="Your Name/"/><br/>
+    Email:<br/>
+    <input type="email" name="email" placeholder="Your Email"/><br/>
+    Message:<br/>
+            <button type="submit">
+                enviar
+            </button>
+
+        </form>
+    </div>
+
+}
+
+export default Form;
